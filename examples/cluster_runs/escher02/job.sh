50699dc16099117052bb0ed9866c8f4bf0120a74
Vitalii Aksenov <aksenov@escher-02.wias-berlin.de>
2026-01-21 13:53:50 +0100

    Added  in job.sh as a workaround to run on cuda12

--- examples/cluster_runs/escher02/job.sh
+++ examples/cluster_runs/escher02/job.sh
@@ -2,6 +2,7 @@
 
 # Configure GPU and memory utilization
 # by setting environment variables
+unset LD_LIBRARY_PATH
 export CUDA_VISIBLE_DEVICES=1
 export XLA_PYTHON_CLIENT_PREALLOCATE="false"
 # Run the targeted python script
